92fa2129c69e758546feac4b545f5fbe10585b92
myeongju00 <j51677767@gmail.com>
2024-08-15 00:35:18 +0900

    frist init

---
+++ src/main/java/com/project/config/BeanConfig.java
@@ -0,0 +1,28 @@
+package com.project.config;
+
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class BeanConfig {
+
+	@Bean
+	DataSource dataSource() {
+		DataSource ds = new DataSource();
+		ds.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+		ds.setUrl("jdbc:oracle:thin:@localhost:1521:XE");
+		ds.setUsername("scott");
+		ds.setPassword("tiger");
+		ds.setInitialSize(5);
+		ds.setMinIdle(5);
+		ds.setMaxIdle(10);
+		ds.setMinEvictableIdleTimeMillis(60000 * 3);
+		ds.setTimeBetweenEvictionRunsMillis(1000 * 10);
+		
+		return ds;
+	}
+	
+	
+}
