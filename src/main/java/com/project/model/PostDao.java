34ea2a648e8a57a9e1841e03b25c5d223af11310
myeongju00 <j51677767@gmail.com>
2024-08-16 16:04:23 +0900

    Feat : 게시글 전체 조회 기능 Dao, SO 로그인 판별 기능 Dao 작성

--- src/main/java/com/project/model/PostDao.java
+++ src/main/java/com/project/model/PostDao.java
@@ -1,14 +1,20 @@
 package com.project.model;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 
 public class PostDao {
-	private JdbcTemplate jdbcTemplate;
-	
+	private final JdbcTemplate jdbcTemplate;
+
+	@Autowired
 	public PostDao(DataSource dataSource) {
 		this.jdbcTemplate = new JdbcTemplate(dataSource);
 	}
@@ -19,5 +25,13 @@ public class PostDao {
 		Number keyValue = keyHolder.getKey();
 		post.setPost_id(keyValue.longValue());
 		//내가 쓴글 기능이 있을 때 필요
+	}
+
+	public List<PostDO> selectAll() {
+		//-- 메인페이지 게시글 조회
+		//SELECT post_id, title, content, position, view_count, created_date FROM post;
+		return this.jdbcTemplate
+				.query("SELECT post_id, title, content, position, view_count, created_date FROM post",
+						new PostRowMapper());
 	}
 }
--- src/main/java/com/project/model/UserDao.java
+++ src/main/java/com/project/model/UserDao.java
@@ -1,13 +1,16 @@
 package com.project.model;
 
+import com.project.exception.WrongIdPasswordException;
 import javax.sql.*;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 public class UserDao {
-	private JdbcTemplate jdbcTemplate;
-	
+	private final JdbcTemplate jdbcTemplate;
+
+	@Autowired
 	public UserDao(DataSource dataSource) {
 		this.jdbcTemplate = new JdbcTemplate(dataSource);
 	}
@@ -26,6 +29,20 @@ public class UserDao {
 	
 	public void update(UserDO user) {
 		jdbcTemplate.update("update userinfo set password = ? where user_id = ?", user.getPassword(), user.getUser_id());
+	}
+
+	public UserDO login(String user_id, String password) {
+		UserDO result;
+		try {
+			result = jdbcTemplate.queryForObject("select * from userinfo where user_id = ? and password = ?",
+					new UserRowMapper(),
+					user_id, password
+			);
+		} catch(EmptyResultDataAccessException e) {
+			throw new WrongIdPasswordException();
+		}
+
+		return result;
 	}
 
 }
