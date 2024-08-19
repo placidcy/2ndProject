57a16f02d753b325bac7c01015141141302a7209
ches409 <ches409@naver.com>
2024-08-19 15:46:09 +0900

    검색 기능 추가

--- src/main/java/com/project/controller/ModelController.java
+++ src/main/java/com/project/controller/ModelController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.project.model.*;
 
@@ -117,7 +118,9 @@ public class ModelController {
 	}
 	
 	@PostMapping("/search")
-	public String searchHandler() {
+	public String searchHandler(@RequestParam(value="searchBar", 
+		    required = false, defaultValue="") String searchValue, HttpSession session) {
+		session.setAttribute("searchPosts", postDao.searchPost(searchValue));
 		return "redirect:/main";
 	}
 	
