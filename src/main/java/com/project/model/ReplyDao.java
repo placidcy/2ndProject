fa4af1db50f4d46565fe0ca77f5e7a4a8b585cb9
jihyun-Park90 <pjh063099@gmail.com>
2024-08-19 09:41:25 +0900

    댓글 입력 기능, 추천수 업데이트 기능 추가

--- src/main/java/com/project/model/ReplyDao.java
+++ src/main/java/com/project/model/ReplyDao.java
@@ -7,6 +7,9 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 
 public class ReplyDao {
 	
@@ -27,9 +30,27 @@ public class ReplyDao {
 	}
 	
 	public void insertReply(ReplyDO reply) {
-		this.sql = "insert into reply(user) values()";
+		this.sql = "insert into reply(reply_id, user_id, post_id, content, create_at, likes) values(seq_reply_reply_id.nextval, ?, ?, ?, sysdate, ?)";
 		
+		KeyHolder keyHorder = new GeneratedKeyHolder();
+		this.jdbcTemplate.update(new PreparedStatementCreator() {
+			
+			@Override
+			public PreparedStatement createPreparedStatement(Connection conn) throws SQLException {
+
+				PreparedStatement pstmt = conn.prepareStatement(sql, new String [] { "reply_id" });
+				pstmt.setString(1, reply.getUser_id());
+				pstmt.setInt(2, reply.getPost_id());
+				pstmt.setString(3, reply.getContent());
+				pstmt.setInt(4, reply.getLikes());
+				return pstmt;
+			}
+		}, keyHorder);
 	}
 	
+	public void updateLikes(int likes, int reply_id) {
+		this.sql = "update reply set likes = ? where reply_id = ?";
+		this.jdbcTemplate.update(sql, likes, reply_id);
+	}
 	
 }
