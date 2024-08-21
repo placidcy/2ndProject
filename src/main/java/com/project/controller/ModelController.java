ded149d6b87ce20ecf0ba851b452d13f018c1022
myeongju00 <j51677767@gmail.com>
2024-08-21 10:56:47 +0900

    Feat : import문 추가

--- src/main/java/com/project/controller/ModelController.java
+++ src/main/java/com/project/controller/ModelController.java
@@ -4,6 +4,7 @@ import com.project.exception.UnExpectedAccessException;
 import com.project.model.PostDO;
 
 
+import com.project.model.ReplyDO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
